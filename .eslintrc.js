c9760e196ca74d72729592538eb821c9755f250a
Jeff Pettis <jeffrey.pettis@gmail.com>
2023-05-23 19:19:34 -0700

    Added jest and react testing configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
+  env: { browser: true, es2020: true, node: true, jest: true },
   extends: [
     'airbnb',
     'airbnb/hooks',
