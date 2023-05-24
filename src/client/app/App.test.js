ca4c54b4048b983e8a80322b39b55c26cc7abfdd
michellesylee <michellebellelee@yahoo.com>
2023-05-23 20:36:11 -0700

    fixed app test with router

--- src/client/app/App.test.js
+++ src/client/app/App.test.js
@@ -1,12 +1,13 @@
 import React from 'React';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 describe('App test', () => {
   let app;
   beforeEach(() => {
-    app = render(<App />);
+    app = render(<App />, { wrapper: BrowserRouter });
   });
 
   it('should render App component', () => {
