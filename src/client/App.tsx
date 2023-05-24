2501ce4c98fb38cb26087b4313edc5d578d9274d
michellesylee <michellebellelee@yahoo.com>
2023-05-23 19:27:45 -0700

    configed basic routes

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,19 @@
+import React, { useState } from 'react';
+import { Router, Routes, Route, useNavigate } from 'react-router-dom';
+import Sellers from './components/Sellers';
+import HomePage from './components/HomePage';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="router">
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sellers" element={<Sellers />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/client/components/HomePage.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState, createContext } from 'react';
+
+function HomePage() {
+  return <div>Hi! This is homepage</div>;
+}
+
+export default HomePage;
