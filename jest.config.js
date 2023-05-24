fa09cd2d4663bf17494ea0bebc609d8c58004928
michellesylee <michellebellelee@yahoo.com>
2023-05-23 19:49:50 -0700

    added routes

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect',
+    './setupJest.js',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/mocks/fileMock.js',
+    '\\.(css)$': './mocks/fileMock.js',
+  },
+};
---
+++ src/client/app/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import HomePage from '../components/HomePage';
+import Sellers from '../components/Sellers';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sellers" element={<Sellers />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
