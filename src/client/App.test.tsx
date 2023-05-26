1191dc0819aeee7e2610807ba0d573bfa3abd846
Jeff Pettis <jeffrey.pettis@gmail.com>
2023-05-25 17:56:54 -0700

    Added redux store config

--- src/client/App.test.tsx
+++ src/client/App.test.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { RenderResult } from '@testing-library/react';
 
 describe('App test', () => {
-  let app;
+  let app: RenderResult;
   beforeEach(() => {
     app = render(<App />, { wrapper: BrowserRouter });
   });
