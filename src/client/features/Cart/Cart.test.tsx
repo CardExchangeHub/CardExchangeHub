bf3a6737c17c6cdb1f8b74dcefef48caf0750b75
Pierce Yu <pierceyu0315@live.com>
2023-05-27 13:00:58 -0400

    cart component functionality completed

--- src/client/features/Cart/Cart.test.tsx
+++ src/client/features/Cart/Cart.test.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Cart } from './Cart';
+import userEvent from '@testing-library/user-event';
 
 describe('Cart component', () => {
   it('should call checkout function when checkout button is clicked', () => {
     const checkoutMock = jest.fn();
-    render(<Cart onCheckout={checkoutMock} />);
+    render(<Cart />);
 
+    //find the checkout button on the screen
     const checkoutButton = screen.getByText('Checkout');
-    fireEvent.click(checkoutButton);
 
+    //simulate button click
+    userEvent.click(checkoutButton);
+
+    //expect the checkout button to be clicked on once
     expect(checkoutMock).toHaveBeenCalledTimes(1);
   });
 });
