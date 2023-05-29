35fd532d4f25b409709c962b9af630b28c175c44
ryan <gauserr@icloud.com>
2023-05-29 19:29:09 -0400

    set up practice db in personal branch. Mainly for practice setting up db by myself and to understand under the hood.

---
+++ src/server/models/practice_db.sql
@@ -0,0 +1,43 @@
+CREATE TABLE "public.Users" (
+	"id" serial NOT NULL,
+	"username" varchar NOT NULL,
+	"password" varchar NOT NULL,
+	CONSTRAINT "Users_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.market_postings" (
+	"id" serial NOT NULL,
+	"condition" VARCHAR(255) NOT NULL,
+	"price" FLOAT NOT NULL,
+	"seller" integer NOT NULL,
+	"cardId" integer NOT NULL,
+	"sold" BOOLEAN NOT NULL DEFAULT 'false',
+	"buyer" integer DEFAULT 'null',
+	CONSTRAINT "market_postings_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.cards" (
+	"id" serial NOT NULL,
+	"brand" VARCHAR(255) NOT NULL,
+	"name" integer NOT NULL,
+	"image" varchar NOT NULL,
+	CONSTRAINT "cards_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "market_postings" ADD CONSTRAINT "market_postings_fk0" FOREIGN KEY ("seller") REFERENCES "Users"("id");
+ALTER TABLE "market_postings" ADD CONSTRAINT "market_postings_fk1" FOREIGN KEY ("cardId") REFERENCES "cards"("id");
+ALTER TABLE "market_postings" ADD CONSTRAINT "market_postings_fk2" FOREIGN KEY ("buyer") REFERENCES "Users"("id");
+
