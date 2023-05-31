fd7f30356062bbb55e8c3c0767746a94eb1b536c
ryan <gauserr@icloud.com>
2023-05-30 20:42:28 -0400

    working on card controller

--- src/server/routes/card_route.ts
+++ src/server/routes/card_route.ts
@@ -3,27 +3,27 @@
 // eslint-disable-next-line import/no-import-module-exports
 import { Response, Request, Router } from 'express';
 // eslint-disable-next-line import/no-import-module-exports
-import cardController from '../controllers/cardController';
+import cardController from '../controllers/cardController.js';
 
 const router = Router();
 
 // get all cards
-router.get('/', cardController.getCards, (req: Request, res: Response) => {
-  res.status(200).json(res.locals.cards);
-});
+// router.get('/', cardController.getCards, (_req: Request, res: Response) => {
+//   res.status(200).json(res.locals.cards);
+// });
 
 // add card
-router.post('/', cardController.addCard, (req: Request, res: Response) => {
-  res.status(200).json(res.locals.cards);
-});
+// router.post('/', cardController.addCard, (req: Request, res: Response) => {
+//   res.status(200).json(res.locals.cards);
+// });
 
 // router.post('/', addCard, (req: Request, res: Response) => {
 //   res.status(200).json(res.locals.cards);
 // });
 
 // edit card
-router.put('/:id', cardController.editCard, (req: Request, res: Response) => {
-  res.status(200).json(res.locals.cards);
-});
+// router.put('/:id', cardController.editCard, (req: Request, res: Response) => {
+//   res.status(200).json(res.locals.cards);
+// });
 
 export default router;
