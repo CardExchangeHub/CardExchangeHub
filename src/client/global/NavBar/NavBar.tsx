4fb461406382a4d499499ddfec757200f07d136f
michellesylee <michellebellelee@yahoo.com>
2023-05-30 20:40:13 -0700

    Revert "adding log in modal"
    This reverts commit 71d5c1ed2c61a6782ca8c2f5d4af6e7c3ff61118.

--- src/client/global/NavBar/NavBar.tsx
+++ src/client/global/NavBar/NavBar.tsx
@@ -4,14 +4,12 @@ import { Link } from 'react-router-dom';
 import { Cart } from '../../features/Cart/Cart';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import useDebounce from '../../hooks/useDebounce';
-import LogIn from '../../components/LogIn/LogIn';
 
 const Navbar: React.FC = () => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cards, setCards] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [isLoading, setLoading] = useState(false);
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false); // State for login modal
 
   const handleCartClick = () => {
     setIsCartOpen(!isCartOpen);
@@ -39,9 +37,6 @@ const Navbar: React.FC = () => {
     setSearchQuery(e.target.value);
     setCards([]);
   };
-  const handleLoginClick = () => {
-    setIsLoginModalOpen(!isLoginModalOpen); // Toggle the login modal state
-  };
 
   return (
     <nav className="flex items-center justify-between bg-gray-800 text-white p-4">
@@ -56,15 +51,38 @@ const Navbar: React.FC = () => {
         />
       </div>
       <div className="flex items-center space-x-4">
-        <button
-          className="hover:text-gray-300"
-          onClick={handleLoginClick} // Toggle the login modal state on click
-        >
+        <Link to="login" className="hover:text-gray-300">
           Log In
-        </button>
-        {/* <Link to="login" className="hover:text-gray-300">
-          Log In
-        </Link> */}
+        </Link>
+        {/* <button className="hover:text-gray-300" onClick={handleCartClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.5}
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+            />{' '}
+          </svg>
+        </button> */}
+        {/* {isCartOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <Cart />
+          <button
+            className="absolute top-4 right-4 text-white"
+            onClick={handleCartClick}
+            aria-label="Close"
+          >
+            Close
+          </button>
+        </div>
+      )} */}
         <Link to="/cart" className="hover:text-gray-300">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -83,7 +101,6 @@ const Navbar: React.FC = () => {
           </svg>
         </Link>
       </div>
-      {isLoginModalOpen && <LogIn />} {/* Render the login modal component */}
     </nav>
   );
 };
