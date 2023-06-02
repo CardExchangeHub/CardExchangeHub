f7de19a3c31e4d4478cf72fd9c73b79de69f5d29
Jeff Pettis <jeffrey.pettis@gmail.com>
2023-06-02 09:30:16 -0700

    added requests for fetch, post, update, delete cards in cardsApi

--- src/client/features/CardsList/cardsApi.ts
+++ src/client/features/CardsList/cardsApi.ts
@@ -7,6 +7,18 @@ interface FetchCardsListParams {
   options: AxiosRequestConfig;
 }
 
+interface PostNewCardParams {
+  userId: string;
+  newCard: Card;
+}
+
+interface UpdateCardParams {
+  cardId: string;
+  card: Card;
+}
+
+// fetch cardslist - _page + _limit (in req.query) - returning limit of "next" 10 most recent cards
+// depending on page number, sorted by date
 export const fetchCardsList = async (
   { page, options }: FetchCardsListParams,
   { rejectWithValue }
@@ -21,23 +33,48 @@ export const fetchCardsList = async (
 };
 
 // UPDATE THIS TO REFLECT THE TCG API - https://pokemontcg.io/
-// We will get data back, display it as we do in cards list, then on click it will populate the 
-// form within the seller dashboard
+// We will get data back, display it as we do in cards list, then on click it will populate the
+// form within the seller dashboard - BACKEND TEAM DO NOT WORRY ABOUT THIS API CALL - Jeff
 export const fetchCardFromPokeApi = async (pokemonName: string) => {
   const response = await pokemon.card.find(pokemonName);
   return response;
 };
 
-export const postNewCard = async (newCard: Card) => {
-
-  const response = await axios.post(`/card/${userId}`, newCard);
-  return response.data;
+// post newCard - userId(req.params) + card (req.body) - return entire card
+export const postNewCard = async (
+  { userId, newCard }: PostNewCardParams,
+  { rejectWithValue }
+) => {
+  try {
+    const response = await axios.post(`/card/${userId}`, newCard);
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
 };
 
-// update card - cardIdDb(params) + body - return entire card
+// update card - cardId(req.params) + card (req.body) - return entire card
+export const updateCard = async (
+  { cardId, card }: UpdateCardParams,
+  { rejectWithValue }
+) => {
+  try {
+    const response = await axios.put(`/card/${cardId}`, card);
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
+};
 
-// delete card - cardIdDb(params) + body - return card id
-
+// delete card - cardId(req.params) - return cardId
+export const deleteCard = async (cardId: string, { rejectWithValue }) => {
+  try {
+    const response = await axios.delete(`/card/${cardId}`);
+    return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
+};
 
 // TEST DATA
 // let cards;
